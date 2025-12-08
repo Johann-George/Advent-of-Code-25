d8d3f10c95c3c3e72a25edb2d79417b9799ff5a0
Johann-George <johanngeorge2003@gmail.com>
2025-12-08 22:39:34 +0530

    Solution for Day 2 part 2

--- Day_2.py
+++ Day_2.py
@@ -4,18 +4,16 @@ def add_invalid_ids():
         for line in file:
             ranges = line.split(",")
             for numbers in ranges:
-                nos = numbers.split("-")
-                for i in range(int(nos[0]), int(nos[1])+1):
-                    num = len(str(i))
-                    if num % 2 == 0:
-                        while num != 0:
-                            string = str(i)[:num]
-                            num = num // 2
-                            if (string[:num] == string[num:]):
+                low, high = map(int, numbers.split("-"))
+                for i in range(low, high + 1):
+                    string = str(i)
+                    num = len(string)
+                    for k in range(1, num):
+                        if num % k == 0:
+                            pattern = string[:k]
+                            if pattern * (num // k) == string:
                                 sum += i
                                 break
-                    else:
-                        continue
     return sum
 
 print(add_invalid_ids())
