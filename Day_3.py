6b1316aea834d51cd7e127bc4b1532bafca6cfff
Johann George <johanngeorge2003@gmail.com>
2025-12-21 23:14:53 +0530

    Day 3 part 2 solution

--- Day_3.py
+++ Day_3.py
@@ -12,7 +12,34 @@ def day_3_part_1_():
 
     return total_joltage
 
+def day_3_part_2_():
+    total_joltage = 0
+    with open("Day_3_input.txt", "r") as file:
+        for line in file:
+            digits = line.strip()
+            stack = []
 
+            to_remove = len(digits) - 12
+            for d in digits:
+                current = int(d)
+
+                while stack and to_remove > 0 and stack[-1] < current:
+                    stack.pop()
+                    to_remove -= 1
+
+                stack.append(current)
+            if to_remove > 0:
+                stack = stack[:-to_remove]
+
+            stack = stack[:12]
+            max_joltage = 0
+            for digit in stack:
+                max_joltage = max_joltage * 10 + digit
+
+            total_joltage += max_joltage
+    return total_joltage
+
+print(day_3_part_2_())
 print(day_3_part_1_())
 
 
