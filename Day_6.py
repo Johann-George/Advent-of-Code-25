fe38b5c9a5d9d8882e69bbaa1453f9df0136c61d
Johann George <johanngeorge2003@gmail.com>
2026-01-04 18:15:12 +0530

    Solved day 6 part 2

--- Day_6.py
+++ Day_6.py
@@ -19,5 +19,42 @@ def day_6_part_1():
         i += 1
     return res
 
+def day_6_part_2():
+    numbers = []
+    total = 0
+    with open('Day_6_input.txt', 'r') as file:
+        numbers = [list(line.rstrip("\n")) for line in file]
+    num_arr = []
+    temp = ""
+    for i in range (len(numbers[0]) - 1, -1, -1):
+        for nums in numbers:
+            if nums[i] == " ":
+                continue
+            elif nums[i] == "+" or nums[i] == "*":
+                if nums[i] == "+":
+                    sum = 0
+                    num_arr.append(temp)
+                    temp = ""
+                    for num in num_arr:
+                        sum += int(num)
+                    print(sum)
+                    total += sum
+                else:
+                    mul = 1
+                    num_arr.append(temp)
+                    for num in num_arr:
+                        mul *= int(num)
+                    print(mul)
+                    total += mul
+                num_arr = []
+                temp = ""
+            else:
+                temp += nums[i] 
+        if temp:
+            num_arr.append(temp)
+            temp = ""
+    return total
+
+print(day_6_part_2())
 print(day_6_part_1())
         
