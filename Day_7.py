47d1edafb282907ebb89af60c8a1fe33af9655c1
Johann George <johanngeorge2003@gmail.com>
2026-01-18 19:37:13 +0530

    Solution for Day 7 part 2

--- Day_7.py
+++ Day_7.py
@@ -1,3 +1,5 @@
+import copy
+
 def day_7_part_1():
     queue = set()
     total_splits = 0
@@ -18,4 +20,44 @@ def day_7_part_1():
                         queue.add(index - 1)
     return total_splits
 
-print(day_7_part_1())
+def day_7_part_2():
+    with open('Day_7_input.txt', 'r') as file:
+        data = [[l for l in line] for line in file]
+    for col in range(len(data[0])):
+        if data[0][col] == 'S':
+            data[1][col] = '|'
+
+    for col in range(len(data[1])):
+        if data[1][col] == '|':
+            beam = col
+            break
+    
+    return dfs(data[1:], 0 , beam)
+
+MEM = {}
+
+def dfs(currState, currLine, beam):
+
+    if(currLine, beam) in MEM:
+        return MEM[(currLine, beam)]
+
+    if currLine == len(currState) - 2:
+        return 1
+    
+    nextLine = currLine + 1
+    if currState[nextLine][beam] == '^':
+        nextState1 = copy.deepcopy(currState) 
+        nextState2 = copy.deepcopy(currState)
+        nextState1[nextLine][beam - 1] = '|'
+        nextState2[nextLine][beam + 1] = '|'
+
+        ans = dfs(nextState1, nextLine, beam-1) + dfs(nextState2, nextLine, beam+1)
+
+    else:
+        currState[nextLine][beam] = '|'
+        ans = dfs(currState, nextLine, beam)
+
+    MEM[(currLine, beam)] = ans
+    return ans
+
+print(day_7_part_2())
